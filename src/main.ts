8b4646f694fac4fcc15d80a7115a1955a5ef79c2
slozhkai <sisipopakaka@gmail.com>
2026-03-10 00:16:48 +0300

    del: add element plus ui kit and manage settings

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import { router } from '@/router/router.ts'
+import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus'
 
 const app = createApp(App)
 
-app
-  .use(createPinia())
-  .use(router)
+app.use(createPinia()).use(router).use(ElementPlus, { size: 'small', zIndex: 3000 })
 
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import ViteFonts from 'unplugin-fonts/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,16 +14,11 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
-    ViteFonts({
-      fontsource: {
-        families: [
-          {
-            name: 'Roboto',
-            weights: [100, 300, 400, 500, 700, 900],
-            styles: ['normal', 'italic'],
-          },
-        ],
-      },
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
     }),
   ],
   resolve: {
