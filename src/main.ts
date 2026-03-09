2abe322f2c341ca1bc45813c6e831837fc529f3f
slozhkai <sisipopakaka@gmail.com>
2026-03-09 23:56:28 +0300

    feat: connect router to main file

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,18 @@ import App from './App.vue'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { router } from '@/router/router.ts'
 
 const app = createApp(App)
 
-app.use(createPinia()).use(
-  createVuetify({
-    components,
-    directives,
-  }),
-)
+app
+  .use(createPinia())
+  .use(
+    createVuetify({
+      components,
+      directives,
+    }),
+  )
+  .use(router)
 
 app.mount('#app')
