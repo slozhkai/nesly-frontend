fdf68e83f9a854fd0054b06d7a2996884bd7a121
slozhkai <sisipopakaka@gmail.com>
2026-03-09 23:44:54 +0300

    feat: add vuetify to main file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(createPinia()).use(
+  createVuetify({
+    components,
+    directives,
+  }),
+)
 
 app.mount('#app')
